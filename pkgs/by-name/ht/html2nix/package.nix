8e678ea7437206b912512481579c80879c114586
reedclanton <clantonreed@gmail.com>
2024-02-27 13:27:33 -0600

    Troubleshooting

--- pkgs/by-name/ht/html2nix/package.nix
+++ pkgs/by-name/ht/html2nix/package.nix
@@ -10,12 +10,25 @@
     hash = "sha256-J0qEBS2I/h1zwf790AvZG0Bqe44YIgc1tgiFm8U41nk=";
   };
 
-  build-system = [
-    python3.pkgs.setuptools-scm
-  ];
-
-  dependencies = [
+#  dependencies = [
 #    python3.pkgs.setuptools
+#    (python3.pkgs.buildPythonPackage rec {
+#      pname = "NetscapeBookmarksFileParser";
+#      version = "1.2";
+#      src = fetchFromGitHub {
+#        owner = "ReedClanton";
+#        repo = "Netscape-Bookmarks-File-Parser";
+#        rev = "v${version}";
+#        hash = "sha256-b4AFTHNMv0aMy25URe22cIAZvAL3pkP0oas//SMWCHY=";
+#      };
+#    })
+#  ];
+
+  # Don't run tests.
+  doCheck = false;
+
+  propagatedBuildInputs = [
+    python3.pkgs.setuptools
     (python3.pkgs.buildPythonPackage rec {
       pname = "NetscapeBookmarksFileParser";
       version = "1.2";
@@ -28,11 +41,8 @@
     })
   ];
 
-  # Don't run tests.
-  doCheck = false;
-
-  nativeBuildInputs = [
-    python3.pkgs.setuptools
+#  nativeBuildInputs = [
+#    python3.pkgs.setuptools
 #    (python3.pkgs.buildPythonPackage rec {
 #      pname = "NetscapeBookmarksFileParser";
 #      version = "1.2";
@@ -43,7 +53,7 @@
 #        hash = "sha256-b4AFTHNMv0aMy25URe22cIAZvAL3pkP0oas//SMWCHY=";
 #      };
 #    })
-  ];
+#  ];
 
   meta = with lib; {
     changelog = "https://github.com/ReedClanton/html2nix/blob/${version}/CHANGELOG.md";
